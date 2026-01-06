030f2abdc07d97d3c410d4ae5bbb6da6aa16e358
Naveen Mahalingam <jedib0t@outlook.com>
2026-01-06 08:27:20 -0800

    passphrase.Generate: use strings.Builder

--- passphrase/generator.go
+++ passphrase/generator.go
@@ -1,7 +1,6 @@
 package passphrase
 
 import (
-	"fmt"
 	"slices"
 	"strings"
 	"unicode"
@@ -44,40 +43,59 @@ func NewGenerator(rules ...Rule) (Generator, error) {
 
 // Generate returns a randomly generated password.
 func (g *generator) Generate() (string, error) {
+	// create and pre-allocate the builder
+	var b strings.Builder
+	// estimate the capacity needed
+	b.Grow(g.wordLenMin*g.numWords + len(g.separator)*(g.numWords-1) + 1)
 
-	// select words
-	var words []string
-	var wordsMap = make(map[string]bool)
+	// inject a random number after one of the words if asked for
+	wordForDigitSuffixIdx, digit := -1, 0
+	if g.withNumber {
+		var err error
+
+		wordForDigitSuffixIdx, err = rng.IntN(g.numWords)
+		if err != nil {
+			return "", err
+		}
+		digit, err = rng.IntN(10)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// append words to the builder
+	var wordIndicesMap = make(map[int]bool)
 	for idx := 0; idx < g.numWords; idx++ {
-		var word string
-		for word == "" || wordsMap[word] {
-			n, err := rng.IntN(g.dictionaryLen)
+		// select a random word index from the dictionary (non-repeating)
+		var wordIndex int
+		var err error
+		for wordIndex == 0 || wordIndicesMap[wordIndex] {
+			wordIndex, err = rng.IntN(g.dictionaryLen)
 			if err != nil {
 				return "", err
 			}
-			word = g.dictionary[n]
 		}
-		words = append(words, word)
-		wordsMap[word] = true
+		wordIndicesMap[wordIndex] = true
+
+		// append the word to the builder
+		b.WriteString(g.dictionary[wordIndex])
+
+		// append the digit to the builder if asked for
+		if wordForDigitSuffixIdx != -1 && idx == wordForDigitSuffixIdx {
+			b.WriteString(string('0' + byte(digit)))
+		}
+
+		// append the separator if not the last word
+		if idx < g.numWords-1 {
+			b.WriteString(g.separator)
+		}
 	}
 
-	// inject a random number after one of the words
-	if g.withNumber {
-		idx, err := rng.IntN(len(words))
-		if err != nil {
-			return "", err
-		}
-		digit, err := rng.IntN(10)
-		if err != nil {
-			return "", err
-		}
-		words[idx] += fmt.Sprint(digit)
-	}
-
-	return strings.Join(words, g.separator), nil
+	return b.String(), nil
 }
 
 func (g *generator) sanitize() (Generator, error) {
+	// check if the word length is valid
 	if g.wordLenMin < 1 || g.wordLenMin > g.wordLenMax {
 		return nil, ErrWordLengthInvalid
 	}
@@ -89,6 +107,7 @@ func (g *generator) sanitize() (Generator, error) {
 	slices.Sort(g.dictionary)
 	g.dictionary = slices.Compact(g.dictionary)
 	g.dictionaryLen = len(g.dictionary)
+
 	// check if the dictionary is too small
 	if g.dictionaryLen < g.numWords || g.dictionaryLen < MinWordsInDictionary {
 		return nil, ErrDictionaryTooSmall
@@ -104,6 +123,7 @@ func (g *generator) sanitize() (Generator, error) {
 		}
 	}
 
+	// check if the number of words is too small or too large
 	if g.numWords < NumWordsMin {
 		return nil, ErrNumWordsTooSmall
 	}
